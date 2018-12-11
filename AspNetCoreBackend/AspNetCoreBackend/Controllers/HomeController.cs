da0866aa7aaee8866fab850459ccc7e37be5bc5e
Jani Järvinen <jani.jarvinen@intertechno.org>
2018-12-11 15:13:27 +0200

    Lisätty uusi action-metodi.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace AspNetCoreBackend.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult OmaTesti()
+        {
+            return Content("Hello, World!");
+        }
     }
 }
