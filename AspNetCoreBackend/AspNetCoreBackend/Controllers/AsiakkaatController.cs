0e8e45f862dd4fe47fbc6ff05b4bbff734bbfc86
Jani Järvinen <jani.jarvinen@intertechno.org>
2018-12-13 14:34:25 +0200

    Lisätty Asiakkas-kontrolleri sekä yksinkertainen asiakaslistaus.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/AsiakkaatController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    public class AsiakkaatController : Controller
+    {
+        public IActionResult Index()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> asiakkaat = context.Customers.ToList();
+
+            return View(asiakkaat);
+        }
+    }
+}
