8c545110283096d418998dfe3ba61dcf7440ce67
Jani Järvinen <jani.jarvinen@intertechno.org>
2018-12-18 11:06:55 +0200

    Lisätty API-kontrolleri projektiin.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Route("merkkijono")]
+        public string Merkkijono()
+        {
+            return "Terve, Joensuu!";
+        }
+
+        [Route("päivämäärä")]
+        public DateTime Päivämäärä()
+        {
+            return DateTime.Now;
+        }
+    }
+}
