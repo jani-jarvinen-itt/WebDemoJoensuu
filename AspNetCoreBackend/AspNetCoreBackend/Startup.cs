bd41003d5b0d43d011596566b27ac80cde40a96e
Jani Järvinen <jani.jarvinen@intertechno.org>
2018-12-20 09:38:08 +0200

    Lisätty CORS-tuki.

--- AspNetCoreBackend/AspNetCoreBackend/Startup.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Startup.cs
@@ -24,6 +24,16 @@ namespace AspNetCoreBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+                options.AddPolicy("MyCorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -38,6 +48,9 @@ namespace AspNetCoreBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // global policy - assign here or on each controller
+            app.UseCors("MyCorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
--- ReactWebFrontEnd/src/App.js
+++ ReactWebFrontEnd/src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    console.log("App-komponentti: konstruktori (rakentaja).");
+
+    this.state = null;
+  }
+
+  componentDidMount() {
+
+    console.log("App-komponentti: componentDidMount-metodissa.");
+    this.setState({ title: "Väliaikainen otsikko"} );
+
+    fetch('https://localhost:44357/api/customers')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+
+        console.log("App-komponentti: aloitetaan setState()-kutsu.");
+        this.setState(json);
+        console.log("App-komponentti: setState()-metodia kutsuttu.");
+
+      });
+
+    console.log("App-komponentti: Fetch-kutsu tehty.");
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+    console.log("App-komponentti: render-metodissa.");
+
+    let otsikko = "";
+    if (this.state != null) {
+      otsikko = this.state.title;
+    }
+
+    return (<div>
+      <h1>App-komponentti</h1>
+      <p>{otsikko}</p>
+    </div>
     );
   }
 }
